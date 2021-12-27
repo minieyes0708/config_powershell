e2db9f0e360403f2538650774478b873b8f0e61f
Minieyes Chen(陳怡哲) <Minieyes_Chen@novatek.com.tw>
2021-12-27 09:06:45 +0800

    add alias to tsvn & tgit

--- Aliases.ps1
+++ Aliases.ps1
@@ -1,3 +1,5 @@
 Set-Alias cpcwd Copy-Path
 Set-Alias cppwd Copy-Path
 Set-Alias cppath Copy-Path
+New-Alias -Force -Name tsvn -Value TortoiseProc.exe
+New-Alias -Force -Name tgit -Value TortoiseGitProc.exe
