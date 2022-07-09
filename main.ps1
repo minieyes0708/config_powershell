c42c7ef37d2c6ace2f0ffd14c21bfc18bff73d95
Minieyes Chen <chenvey2@gmail.com>
2022-07-10 00:40:28 +0800

    Unblock files

--- main.ps1
+++ main.ps1
@@ -1,3 +1,8 @@
+Unblock-File -Path ~/.config/powershell/Env.ps1
+Unblock-File -Path ~/.config/powershell/PSFzf.ps1
+Unblock-File -Path ~/.config/powershell/Aliases.ps1
+Unblock-File -Path ~/.config/powershell/Imports.ps1
+Unblock-File -Path ~/.config/powershell/oh-my-posh.ps1
 . ~/.config/powershell/Env.ps1
 . ~/.config/powershell/PSFzf.ps1
 . ~/.config/powershell/Aliases.ps1
