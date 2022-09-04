b10456d00f7b112f2de2f0c1a0c2c3fb9973707e
Minieyes Chen <chenvey2@gmail.com>
2022-09-04 09:40:53 +0800

    add alias to lazygit(lg)

--- Aliases.ps1
+++ Aliases.ps1
@@ -2,5 +2,6 @@ Set-Alias sf Start-File
 Set-Alias cpcwd Copy-Path
 Set-Alias cppwd Copy-Path
 Set-Alias cppath Copy-Path
+New-Alias -Force -Name lg -Value lazygit
 New-Alias -Force -Name tsvn -Value TortoiseProc.exe
 New-Alias -Force -Name tgit -Value TortoiseGitProc.exe
