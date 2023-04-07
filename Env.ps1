d086d70a3becde8e60d4bfe47856e21822e49482
minieyes <chenvey2@gmail.com>
2023-04-07 22:28:53 +0800

    add more to PATH

--- Env.ps1
+++ Env.ps1
@@ -1,4 +1,5 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding($False)
 
-$env:PATH = "${env:PATH};C:\Users\nvt02863\AppData\Local\Programs\oh-my-posh\bin\"
+$env:PATH = "${env:PATH};C:\Users\${env:USERNAME}\AppData\Local\Programs\oh-my-posh\bin\"
+$env:PATH = "${env:PATH};C:\msys64\usr\bin\"
 $env:FZF_DEFAULT_OPTS = '--height=40% --reverse'
