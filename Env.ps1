35706d42803904d3aafc39b2687782046206e32a
Minieyes Chen <chenvey2@gmail.com>
2023-12-09 21:51:04 +0800

    add some paths

--- Env.ps1
+++ Env.ps1
@@ -1,5 +1,8 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding($False)
 
 $env:PATH = "${env:PATH};C:\Users\${env:USERNAME}\AppData\Local\Programs\oh-my-posh\bin\"
-$env:PATH = "${env:PATH};C:\msys64\usr\bin\"
+$env:PATH = "${env:PATH};D:\minieyes\software\nvim-win64\bin"
+$env:PATH = "${env:PATH};C:\ProgramData\chocolatey\bin"
+$env:PATH = "${env:PATH};C:\Program Files\CMake\bin"
+$env:PATH = "${env:PATH};C:\Program Files\7-Zip"
 $env:FZF_DEFAULT_OPTS = '--height=40% --reverse'
