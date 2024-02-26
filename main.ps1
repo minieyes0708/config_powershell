007050ee8cd8a10ef9e5c0f8aac08bb47b54d342
Minieyes Chen <chenvey2@gmail.com>
2024-02-26 21:35:45 +0800

    source from DotConfig

--- main.ps1
+++ main.ps1
@@ -1,10 +1,10 @@
-Unblock-File -Path ~/.config/powershell/Env.ps1
-Unblock-File -Path ~/.config/powershell/PSFzf.ps1
-Unblock-File -Path ~/.config/powershell/Aliases.ps1
-Unblock-File -Path ~/.config/powershell/Imports.ps1
-Unblock-File -Path ~/.config/powershell/oh-my-posh.ps1
-. ~/.config/powershell/Env.ps1
-. ~/.config/powershell/PSFzf.ps1
-. ~/.config/powershell/Aliases.ps1
-. ~/.config/powershell/Imports.ps1
-. ~/.config/powershell/oh-my-posh.ps1
+Unblock-File -Path $env:DotConfig/powershell/Env.ps1
+Unblock-File -Path $env:DotConfig/powershell/PSFzf.ps1
+Unblock-File -Path $env:DotConfig/powershell/Aliases.ps1
+Unblock-File -Path $env:DotConfig/powershell/Imports.ps1
+Unblock-File -Path $env:DotConfig/powershell/oh-my-posh.ps1
+. $env:DotConfig/powershell/Env.ps1
+. $env:DotConfig/powershell/PSFzf.ps1
+. $env:DotConfig/powershell/Aliases.ps1
+. $env:DotConfig/powershell/Imports.ps1
+. $env:DotConfig/powershell/oh-my-posh.ps1
