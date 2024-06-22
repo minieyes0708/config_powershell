a7098716f5e16ad0fee60f29a54f5ad35bb59769
Minieyes Chen <chenvey2@gmail.com>
2024-06-22 14:05:35 +0800

    PSFzf put in function to be invoked later

--- PSFzf.ps1
+++ PSFzf.ps1
@@ -1,4 +1,5 @@
-Import-Module PSFzf
-Set-PsFzfOption -TabExpansion
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+function Initialize-PsFZF {
+    Import-Module PSFzf
+    Set-PsFzfOption -TabExpansion
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+}
