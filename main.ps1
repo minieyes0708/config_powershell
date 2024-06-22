a49497eab221f4934d48b3dd93057e7bd2e461bd
Minieyes Chen <chenvey2@gmail.com>
2024-06-22 14:06:21 +0800

    call all ps1 files in DotConfig, add Initialize-Modules function

--- main.ps1
+++ main.ps1
@@ -1,10 +1,10 @@
-Unblock-File -Path $env:DotConfig/powershell/Env.ps1
-Unblock-File -Path $env:DotConfig/powershell/PSFzf.ps1
-Unblock-File -Path $env:DotConfig/powershell/Aliases.ps1
-Unblock-File -Path $env:DotConfig/powershell/Imports.ps1
-Unblock-File -Path $env:DotConfig/powershell/oh-my-posh.ps1
-. $env:DotConfig/powershell/Env.ps1
-. $env:DotConfig/powershell/PSFzf.ps1
-. $env:DotConfig/powershell/Aliases.ps1
-. $env:DotConfig/powershell/Imports.ps1
-. $env:DotConfig/powershell/oh-my-posh.ps1
+foreach ($file in Get-ChildItem $env:DotConfig/powershell/*.ps1) {
+    if (-not ($file.Name -eq 'main.ps1')) {
+        Unblock-File -Path $file; . $file
+    }
+}
+function Initialize-Modules {
+    Set-RandomPrompt
+    Initialize-PSReadLine
+    Import-Module -Name Terminal-Icons
+}
