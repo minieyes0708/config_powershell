80f8c247699dac42532a1ed70e7138743a0ed4c5
Minieyes Chen <chenvey2@gmail.com>
2024-06-22 14:06:32 +0800

    remove unused aliases, add alias ini

--- Aliases.ps1
+++ Aliases.ps1
@@ -1,8 +1,5 @@
 Set-Alias sf Start-File
-Set-Alias ?? chatgpt.ps1
-Set-Alias cpcwd Copy-Path
-Set-Alias cppwd Copy-Path
-Set-Alias cppath Copy-Path
+Set-Alias ini Initialize-Modules
 New-Alias -Force -Name lg -Value lazygit
 New-Alias -Force -Name tsvn -Value TortoiseProc.exe
 New-Alias -Force -Name tgit -Value TortoiseGitProc.exe
