4cc548e50bf4099e581c6fa353beeb428b59aecc
Minieyes Chen <chenvey2@gmail.com>
2024-06-22 14:05:51 +0800

    rename on-my-posh function names to use valid verbs

--- oh-my-posh.ps1
+++ oh-my-posh.ps1
@@ -1,24 +1,12 @@
-function Random-Prompt {
+function Set-RandomPrompt {
     $files = (Get-ChildItem $env:DotConfig/oh-my-posh/themes)
     $index = Get-Random -Maximum ($files.Count)
     $env:OhMyPoshTheme = $files[$index - 1]
     oh-my-posh --init --shell pwsh --config $env:OhMyPoshTheme | Invoke-Expression
 }
-function Backup-OhMyPoshTheme {
-    mv $env:OhMyPoshTheme $env:DotConfig/oh-my-posh/themes_backup/
+function Move-OhMyPoshThemeToBackup {
+    Move-Item $env:OhMyPoshTheme $env:DotConfig/oh-my-posh/themes_backup/
 }
 function Remove-OhMyPoshTheme {
-    rm $env:OhMyPoshTheme
-}
-switch ("$env:COMPUTERNAME-$env:USERNAME")
-{
-    "MINIEYESNB1-怡哲"
-    {
-        # Random-Prompt
-        # oh-my-posh fails on win8
-    }
-    default
-    {
-        Random-Prompt
-    }
+    Remove-Item $env:OhMyPoshTheme
 }
