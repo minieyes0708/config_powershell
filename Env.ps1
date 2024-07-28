0d52c865ddab7cdf5b56c0265b08a12ccea200f1
Minieyes Chen <chenvey2@gmail.com>
2024-07-28 21:42:41 +0800

    Env.ps1 put in DotConfig

--- Env.ps1
+++
@@ -1,7 +0,0 @@
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding($False)
-
-$env:PATH = "${env:PATH};C:\Users\${env:USERNAME}\AppData\Local\Programs\oh-my-posh\bin\"
-$env:PATH = "${env:PATH};D:\minieyes\software\nvim-win64\bin"
-$env:PATH = "${env:PATH};C:\ProgramData\chocolatey\bin"
-$env:PATH = "${env:PATH};C:\Program Files\CMake\bin"
-$env:PATH = "${env:PATH};C:\Program Files\7-Zip"
