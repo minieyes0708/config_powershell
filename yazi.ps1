c47d090d035be547349afe7f5073dbe075b431a3
Minieyes Chen <chenvey2@gmail.com>
2024-07-28 21:53:11 +0800

    add yy function

--- yazi.ps1
+++ yazi.ps1
@@ -1,1 +1,11 @@
 $env:YAZI_FILE_ONE = 'C:\Program Files\Git\usr\bin\file.exe'
+
+function yy {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
