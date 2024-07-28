ace58422b505e562a4e33f5e838d9b13f30b3020
Minieyes Chen <chenvey2@gmail.com>
2024-07-28 21:47:55 +0800

    oh-my-posh path move to env:PROGRAM_PATH

--- oh-my-posh.ps1
+++ oh-my-posh.ps1
@@ -1,11 +1,11 @@
 function Set-RandomPrompt {
-    $files = (Get-ChildItem $env:DotConfig/oh-my-posh/themes)
+    $files = (Get-ChildItem $env:PROGRAM_PATH/oh-my-posh/themes)
     $index = Get-Random -Maximum ($files.Count)
     $env:OhMyPoshTheme = $files[$index - 1]
     oh-my-posh --init --shell pwsh --config $env:OhMyPoshTheme | Invoke-Expression
 }
 function Move-OhMyPoshThemeToBackup {
-    Move-Item $env:OhMyPoshTheme $env:DotConfig/oh-my-posh/themes_backup/
+    Move-Item $env:OhMyPoshTheme $env:PROGRAM_PATH/oh-my-posh/themes_backup/
 }
 function Remove-OhMyPoshTheme {
     Remove-Item $env:OhMyPoshTheme
