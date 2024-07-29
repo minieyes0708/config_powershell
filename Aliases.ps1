a51e0429e353d5c94896520011fa93e6f48258fe
Minieyes Chen <chenvey2@gmail.com>
2024-07-29 23:12:44 +0800

    add some functions for shorter command alias

--- Aliases.ps1
+++ Aliases.ps1
@@ -3,3 +3,7 @@ Set-Alias init Initialize-Modules
 New-Alias -Force -Name lg -Value lazygit
 New-Alias -Force -Name tsvn -Value TortoiseProc.exe
 New-Alias -Force -Name tgit -Value TortoiseGitProc.exe
+
+function p { x p $args }
+function sf { x sf $args }
+function dict { x dict $args }
