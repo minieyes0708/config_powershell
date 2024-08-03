fb325bdd573eaa55117bb7c8e92131b5bc8f447f
Minieyes Chen <chenvey2@gmail.com>
2024-08-03 18:24:50 +0800

    rename env:PROGRAM_PATH to env:ProgramPath

--- oh-my-posh.ps1
+++ oh-my-posh.ps1
@@ -1,17 +1,17 @@
 function Set-RandomPrompt {
-    $files = (Get-ChildItem $env:PROGRAM_PATH/oh-my-posh/themes)
+    $files = (Get-ChildItem $env:ProgramPath/oh-my-posh/themes)
     $index = Get-Random -Maximum ($files.Count)
     $env:OhMyPoshTheme = $files[$index - 1]
     oh-my-posh --init --shell pwsh --config $env:OhMyPoshTheme | Invoke-Expression
 }
 function Select-OhMyPoshTheme {
-    $folder = "$($env:PROGRAM_PATH)/oh-my-posh/themes"
+    $folder = "$($env:ProgramPath)/oh-my-posh/themes"
     $filenames = Get-ChildItem $folder | ForEach-Object { $_.Name }
     $env:OhMyPoshTheme = Join-Path $($folder) $($filenames | fzf)
     oh-my-posh --init --shell pwsh --config $env:OhMyPoshTheme | Invoke-Expression
 }
 function Move-OhMyPoshThemeToBackup {
-    Move-Item $env:OhMyPoshTheme $env:PROGRAM_PATH/oh-my-posh/themes_backup/
+    Move-Item $env:OhMyPoshTheme $env:ProgramPath/oh-my-posh/themes_backup/
 }
 function Remove-OhMyPoshTheme {
     Remove-Item $env:OhMyPoshTheme
