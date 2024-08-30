ea16654e3ae96781d7d9cbed3d15d2975c646ac7
Minieyes Chen <chenvey2@gmail.com>
2024-08-30 23:49:28 +0800

    add FZF_ALT_C_COMMAND

--- PSFzf.ps1
+++ PSFzf.ps1
@@ -1,4 +1,6 @@
 function Initialize-PsFZF {
+    $env:FZF_ALT_C_COMMAND='fd -t d'
+
     Import-Module PSFzf
     Set-PsFzfOption -TabExpansion
     Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
