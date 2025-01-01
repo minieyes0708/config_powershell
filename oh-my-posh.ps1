32b239801d6ecc237c7350bb7848bb86933e8bd6
minieyes <chenvey2@gmail.com>
2025-01-01 17:14:09 +0800

    oh-my-posh.ps1 prompt now emmits 033 OSC command

--- oh-my-posh.ps1
+++ oh-my-posh.ps1
@@ -3,12 +3,36 @@ function Set-RandomPrompt {
     $index = Get-Random -Maximum ($files.Count)
     $env:OhMyPoshTheme = $files[$index - 1]
     oh-my-posh --init --shell pwsh --config $env:OhMyPoshTheme | Invoke-Expression
+
+    $script:OhMyPoshPromptFunction = $Function:prompt
+    $Function:prompt = {
+        $p = $executionContext.SessionState.Path.CurrentLocation
+        $osc7 = ""
+        if ($p.Provider.Name -eq "FileSystem") {
+            $ansi_escape = [char]27
+            $provider_path = $p.ProviderPath -Replace "\\", "/"
+            $osc7 = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}${ansi_escape}\"
+        }
+        "${osc7}" + $(Invoke-Command -ScriptBlock $script:OhMyPoshPromptFunction)
+    }
 }
 function Select-OhMyPoshTheme {
     $folder = "$($env:ProgramPath)/oh-my-posh/themes"
     $filenames = Get-ChildItem $folder | ForEach-Object { $_.Name }
     $env:OhMyPoshTheme = Join-Path $($folder) $($filenames | fzf)
     oh-my-posh --init --shell pwsh --config $env:OhMyPoshTheme | Invoke-Expression
+
+    $script:OhMyPoshPromptFunction = $Function:prompt
+    $Function:prompt = {
+        $p = $executionContext.SessionState.Path.CurrentLocation
+        $osc7 = ""
+        if ($p.Provider.Name -eq "FileSystem") {
+            $ansi_escape = [char]27
+            $provider_path = $p.ProviderPath -Replace "\\", "/"
+            $osc7 = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}${ansi_escape}\"
+        }
+        "${osc7}" + $(Invoke-Command -ScriptBlock $script:OhMyPoshPromptFunction)
+    }
 }
 function Move-OhMyPoshThemeToBackup {
     Move-Item $env:OhMyPoshTheme $env:ProgramPath/oh-my-posh/themes_backup/
